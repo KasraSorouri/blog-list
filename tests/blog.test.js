f14c9d916c06c46758fedbb963a324e1c28bb57b
kasra <k.sorouri@gmail.com>
2022-11-21 20:47:34 +0200

    End of Exercise 4.7

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -139,3 +139,27 @@ describe('most bolgs', () => {
         })
     })
 })
+
+describe('most likes', () => {
+
+  test('of empty list is zero', () => {
+      const result = listHelper.mostLikes([])
+      expect(result).toEqual(null)
+  })
+  
+  test('when list has only one blog, reply the blog', () => {
+      const result = listHelper.mostLikes(listWithOneBlog)
+      expect(result).toEqual({
+          author: 'Edsger W. Dijkstra',
+          likes: 5
+      })
+  })
+  
+  test('of bigger list is calculated right ', () => {
+      const result = listHelper.mostLikes(blogs)
+      expect(result).toEqual({
+        author: "Edsger W. Dijkstra",
+        likes: 17
+      })
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -53,9 +53,38 @@ const mostBlogs = (blogs) => {
     return most
 }
 
+const mostLikes = (blogs) => {
+    let authors = (blogs.length > 0 ? [{author:'', likes: 0}] : null)
+
+    if (blogs.length > 0) {
+      blogs.map(blog => {
+        const author= authors.find(author => author.author === blog.author)
+//        console.log('find reapeted author ->', author);
+        if (author) {
+            author.likes += blog.likes
+        } else {
+            authors = authors.concat({author: blog.author, likes: blog.likes})
+ //           console.log('39 * authors ->',authors)
+        }
+      })
+    }
+    const most = authors ? {author: '', likes: 0} : null
+    if (most) {
+      authors.map(author => {
+        if (author.likes > most.likes) {
+            most.author = author.author,
+            most.likes = author.likes
+        }
+    })
+}
+    console.log('most ->', most)
+    return most
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
