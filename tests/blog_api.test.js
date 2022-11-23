70cb98f92868eff6a03111f48b696c05b0e26096
kasra <k.sorouri@gmail.com>
2022-11-24 00:59:48 +0200

    End of Exercise 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const blogTester = require('./test_helper')
 const Blog  = require('../models/blog')
+const { response } = require('../app')
 
 const api = supertest(app)
 
@@ -79,12 +80,28 @@ describe('post new item', () => {
         await api
             .post('/api/blogs')
             .send(blogObject)
-            
+
         const blogs = await api.get('/api/blogs')
         expect(blogs.body.length).toBe(initialBlogs.body.length+1)
         
         const urls = blogs.body.map(blog => blog.url)
         expect(urls).toContain('http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html')
+    })
+})
+
+describe('default Value', () => {
+    test('default value for like is zero', async () => {
+        const blogObject = {
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html"
+        }
+
+        const response = await api
+            .post('/api/blogs')
+            .send(blogObject)
+            const blog = await Blog.findOne({url:'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'})
+            expect(blog.likes).toBe(0)
     })
 })
 
